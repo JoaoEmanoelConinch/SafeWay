cacdc870874fbaa2490d39b2defe2922aef6f741
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-09-01 20:15:10 -0300

    Removendo o tipo espasial de Ponto

--- target/generated-sources/annotations/modelo/entidade/mapa/Ponto_.java
+++ target/generated-sources/annotations/modelo/entidade/mapa/Ponto_.java
@@ -3,17 +3,18 @@ package modelo.entidade.mapa;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
-import org.geojson.Point;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Ponto.class)
 public abstract class Ponto_ {
 
 	public static volatile SingularAttribute<Ponto, Long> idPonto;
-	public static volatile SingularAttribute<Ponto, Point> LongLatAlt;
+	public static volatile SingularAttribute<Ponto, Double> latitude;
+	public static volatile SingularAttribute<Ponto, Double> longitude;
 
 	public static final String ID_PONTO = "idPonto";
-	public static final String LONG_LAT_ALT = "LongLatAlt";
+	public static final String LATITUDE = "latitude";
+	public static final String LONGITUDE = "longitude";
 
 }
 
