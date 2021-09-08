2165b93da20de32ff2a0056c9e90988c660a5ea9
WesleyDePaula <87441035+WesleyDePaula@users.noreply.github.com>
2021-09-08 17:21:14 -0300

    Update Principal.java

--- src/main/java/controlador/Principal.java
+++ src/main/java/controlador/Principal.java
@@ -1,9 +1,16 @@
 package controlador;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+import modelo.dao.Trajeto.TrajetoDAOImpl;
 import modelo.dao.Usuario.UsuarioDAOImpl;
+import modelo.enumeracao.mapa.*;
+import modelo.entidade.mapa.Trajeto;
 import modelo.entidade.usuario.UsuarioCadastrado;
 import modelo.excecao.mapa.StatusInvalidoException;
 import modelo.excecao.usuario.EmailInvalidoException;
@@ -12,13 +19,24 @@ import modelo.excecao.usuario.StringVaziaException;
 
 public class Principal {
 
-	public static void main(String[] args) throws StringVaziaException, EmailInvalidoException, SenhaPequenaException, JsonMappingException, JsonProcessingException, StatusInvalidoException {
+	public static void main(String[] args) throws StringVaziaException, EmailInvalidoException, SenhaPequenaException, StatusInvalidoException, JsonParseException, org.codehaus.jackson.map.JsonMappingException, IOException {
 		
 		UsuarioCadastrado usuario = new UsuarioCadastrado("Jorge", "golfinho456", "JorgeJohnson@gmail.com");
 		UsuarioDAOImpl daoUsuario = new UsuarioDAOImpl();
-		daoUsuario.inserirUsuario(usuario);
 		
 
+		
+		Trajeto ponto = new Trajeto("-26.4567", "9.12344", MeioDeTransporte.DRIVING_CAR);
+		TrajetoDAOImpl daoTrajeto= new TrajetoDAOImpl();
+		
+		daoTrajeto.inserirTrajeto(ponto);
+		
+//		List<Ponto> pontos = daoPonto.recuperarPontos();
+//		
+//		for(Ponto pontosRecuperados : pontos) {
+//			System.out.println("Latitude: "+pontosRecuperados.getLatitude());
+//			System.out.println("Longitude: "+pontosRecuperados.getLongitude());
+//		}
 
 		
 	}
