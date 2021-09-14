07b7a32e1d898ca3103dce403741871f69801023
WesleyDePaula <87441035+WesleyDePaula@users.noreply.github.com>
2021-09-14 16:01:37 -0300

    Update Principal.java

--- src/main/java/controlador/Principal.java
+++ src/main/java/controlador/Principal.java
@@ -1,19 +1,19 @@
 package controlador;
 
-import java.util.List;
+import java.io.IOException;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 
 import modelo.dao.Formulario.FormularioDAOImpl;
 import modelo.dao.Ponto.PontoDAOImpl;
 import modelo.dao.PontoAvaliado.PontoAvaliadoDAOImpl;
 import modelo.dao.PontoFavorito.PontoFavDAOImpl;
+import modelo.dao.Trajeto.TrajetoDAOImpl;
 import modelo.dao.Usuario.UsuarioDAOImpl;
 import modelo.entidade.mapa.Ponto;
-import modelo.entidade.mapa.PontoFavorito;
-import modelo.entidade.usuario.UsuarioCadastrado;
-
+import modelo.entidade.mapa.Trajeto;
+import modelo.enumeracao.mapa.MeioDeTransporte;
 import modelo.excecao.mapa.StatusInvalidoException;
 import modelo.excecao.usuario.EmailInvalidoException;
 import modelo.excecao.usuario.SenhaPequenaException;
@@ -21,28 +21,24 @@ import modelo.excecao.usuario.StringVaziaException;
 
 public class Principal {
 
-	public static void main(String[] args) throws StringVaziaException, EmailInvalidoException, SenhaPequenaException,
-
-			JsonMappingException, JsonProcessingException, StatusInvalidoException {
+	public static void main(String[] args) throws StringVaziaException, EmailInvalidoException, SenhaPequenaException, StatusInvalidoException, JsonParseException, JsonMappingException, IOException {
 
 		UsuarioDAOImpl usuarioDao = new UsuarioDAOImpl();
 		PontoDAOImpl pontoDao = new PontoDAOImpl();
 		FormularioDAOImpl formDao = new FormularioDAOImpl();
 		PontoAvaliadoDAOImpl pontoAvDao = new PontoAvaliadoDAOImpl();
 		PontoFavDAOImpl pontoFavDao = new PontoFavDAOImpl();
-		
-		
-		UsuarioCadastrado usuario = new UsuarioCadastrado(2);
-		List<UsuarioCadastrado> usuario1 = usuarioDao.recuperarUsuario(usuario);
-		UsuarioCadastrado usuario2 = usuario1.get(0);
-		
-		usuarioDao.deletarUsuario(usuario2);
-		
-//		usuarioDao.inserirUsuario(usuario);
+		TrajetoDAOImpl trajetoDao = new TrajetoDAOImpl();
 
-		
-		
-		
+		Ponto p1 = new Ponto(-26.456, 12.212);
+		Ponto p2 = new Ponto(-26.789, 11.956);
+
+//		pontoDao.inserirPonto(p1);
+//		pontoDao.inserirPonto(p2);
+
+		Trajeto trajeto = new Trajeto(p1, p2, MeioDeTransporte.FOOT_WALKING);
+
+//		trajetoDao.inserirTrajeto(trajeto);
 
 	}
 }
