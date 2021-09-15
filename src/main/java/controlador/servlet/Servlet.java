ea14d7ab56c2eb2de766a9d7bf129399f8aeb4ef
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-09-15 16:03:04 -0300

    Alterando projeto para Servlet

---
+++ src/main/java/controlador/servlet/Servlet.java
@@ -0,0 +1,9 @@
+package controlador.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/")
+public class Servlet extends HttpServlet{
+
+}
