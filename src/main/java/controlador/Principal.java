0b158a443e61f9f6cd85018d5a1d3de76ef72490
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-09-16 21:35:49 -0300

    testando daos

--- src/main/java/controlador/Principal.java
+++ src/main/java/controlador/Principal.java
@@ -1,8 +1,6 @@
 package controlador;
 
 import java.io.IOException;
-import java.util.List;
-
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -10,16 +8,16 @@ import org.codehaus.jackson.map.JsonMappingException;
 import modelo.dao.Formulario.FormularioDAOImpl;
 import modelo.dao.Ponto.PontoDAOImpl;
 import modelo.dao.PontoAvaliado.PontoAvaliadoDAOImpl;
+import modelo.dao.PontoFavorito.PontoFavDAO;
 import modelo.dao.PontoFavorito.PontoFavDAOImpl;
 import modelo.dao.Trajeto.TrajetoDAOImpl;
 import modelo.dao.Usuario.UsuarioDAOImpl;
 import modelo.entidade.formulario.Formulario;
 import modelo.entidade.mapa.Ponto;
-import modelo.entidade.mapa.PontoFavorito;
+import modelo.entidade.mapa.PontoAvaliado;
 import modelo.entidade.mapa.Trajeto;
 import modelo.entidade.usuario.UsuarioCadastrado;
-import modelo.enumeracao.mapa.MeioDeTransporte;
-
+import modelo.excecao.mapa.NumeroMenorQueZeroException;
 import modelo.excecao.mapa.StatusInvalidoException;
 import modelo.excecao.usuario.EmailInvalidoException;
 import modelo.excecao.usuario.SenhaPequenaException;
@@ -27,29 +25,28 @@ import modelo.excecao.usuario.StringVaziaException;
 
 public class Principal {
 
-	public static void main(String[] args) throws StringVaziaException, EmailInvalidoException, SenhaPequenaException, StatusInvalidoException, JsonParseException, JsonMappingException, IOException{
+	public static void main(String[] args) throws StringVaziaException, EmailInvalidoException, SenhaPequenaException, StatusInvalidoException, JsonParseException, JsonMappingException, IOException, NumeroMenorQueZeroException{
 		
-		PontoDAOImpl pontoDAOImpl = new PontoDAOImpl();
-		TrajetoDAOImpl trajetoDAOImpl = new TrajetoDAOImpl();
-
-		Ponto p1 = new Ponto(8.681495,49.41461);
-		Ponto p2 = new Ponto(8.687872,49.420318);
-
-		pontoDAOImpl.inserirPonto(p1);
-		pontoDAOImpl.inserirPonto(p2);
+		PontoDAOImpl pontoDAOImpl                 = new PontoDAOImpl();
+		UsuarioDAOImpl usuarioDAOImpl             = new UsuarioDAOImpl();
+		TrajetoDAOImpl trajetoDAOImpl             = new TrajetoDAOImpl();
+		PontoFavDAOImpl pontoFavDAOImpl           = new PontoFavDAOImpl();
+		FormularioDAOImpl formularioDAOImpl       = new FormularioDAOImpl();
+		PontoAvaliadoDAOImpl pontoAvaliadoDAOImpl = new PontoAvaliadoDAOImpl();
 		
-		Trajeto t = new Trajeto();
-		t.setInicio(p1);
-		t.setChegada(p2);
-		t.setTransporteUsado(MeioDeTransporte.DRIVING_CAR);
 
-		// for (int i = 0; i < t.getPontos().size(); i++){
-		// 	Ponto ponto = new Ponto(t.getPontos().get(i).getLatitude(), t.getPontos().get(i).getLongitude());
-		// 	pontoDAOImpl.inserirPonto(ponto);
-		// }
+		System.out.println();
+		Ponto p1 = pontoDAOImpl.recuperarPontos().get(0);
+		Ponto p2 = pontoDAOImpl.recuperarPontos().get(1);
+		
+		UsuarioCadastrado u1 = usuarioDAOImpl.recuperarUsuario(new UsuarioCadastrado(1));
+		UsuarioCadastrado u2 = usuarioDAOImpl.recuperarUsuario(new UsuarioCadastrado(2));
 
-		trajetoDAOImpl.inserirTrajeto(t);
+		Formulario f = formularioDAOImpl.recuperarAvaliacaoId(new Formulario(1));
 
+		PontoAvaliado pa1 = new PontoAvaliado(p2, f);
+
+		//pontoAvaliadoDAOImpl.adicionarPontoAvaliado(pa1);
 
 	}
 }
