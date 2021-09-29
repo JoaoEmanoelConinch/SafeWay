7e030c4bdb813c498f67f770f44e9fbe43cfadba
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-09-28 21:04:54 -0300

    Update PontoAvaliado.java

--- src/main/java/modelo/entidade/mapa/PontoAvaliado.java
+++ src/main/java/modelo/entidade/mapa/PontoAvaliado.java
@@ -27,38 +27,38 @@ public class PontoAvaliado extends PontoAbstrato{
 
 	private static final long serialVersionUID = 1L;
 
-		@OneToMany(
-	        mappedBy = "idPontoAvaliado",
-	        cascade = CascadeType.ALL,
-	        orphanRemoval = true)
-		private List<Formulario> avaliacoes;
+	@OneToMany(
+        mappedBy = "idPontoAvaliado",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true)
+	private List<Formulario> avaliacoes;
 
-		@Column(name = "quantidade_lesoes_corporais_ponto_avaliado", nullable = false)
-		private long quantidadeLesoesCorporais;
-		
-		@Column(name = "quantidade_furtos_ponto_avaliado", nullable = false)
-		private long quantidadeFurtos;
+	@Column(name = "quantidade_lesoes_corporais_ponto_avaliado", nullable = false)
+	private long quantidadeLesoesCorporais;
+	
+	@Column(name = "quantidade_furtos_ponto_avaliado", nullable = false)
+	private long quantidadeFurtos;
 
-		@Column(name = "quantidade_roubos_ponto_avaliado", nullable = false)
-		private long quantidadeRoubos;
+	@Column(name = "quantidade_roubos_ponto_avaliado", nullable = false)
+	private long quantidadeRoubos;
 
-		@Column(name = "quantidade_homicidios_ponto_avaliado", nullable = false)
-		private long quantidadeHomicidios;
+	@Column(name = "quantidade_homicidios_ponto_avaliado", nullable = false)
+	private long quantidadeHomicidios;
 
-		@Column(name = "quantidade_latrocinio_ponto_avaliado", nullable = false)
-		private long quantidadeLatrocinio;
+	@Column(name = "quantidade_latrocinio_ponto_avaliado", nullable = false)
+	private long quantidadeLatrocinio;
 
-		@Column(name = "nivel_Bloqueio_Ponto_Avaliado", nullable = false)
-		private boolean bloqueio;
+	@Column(name = "nivel_Bloqueio_Ponto_Avaliado", nullable = false)
+	private boolean bloqueio;
 
-		@Column(name = "media_Avaliacao_Ponto_Avaliado", nullable = false)
-		private double mediaDeAvaliacao;
+	@Column(name = "media_Avaliacao_Ponto_Avaliado", nullable = false)
+	private double mediaDeAvaliacao;
 
-		@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
-		@OnDelete(action = OnDeleteAction.NO_ACTION)
-	    @MapsId
-	    @JoinColumn(name = "id_ponto")
-		private Ponto ponto;		
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+	@OnDelete(action = OnDeleteAction.NO_ACTION)
+	@MapsId
+	@JoinColumn(name = "id_ponto")
+	private Ponto ponto;
 
 	public PontoAvaliado() {}
 	
