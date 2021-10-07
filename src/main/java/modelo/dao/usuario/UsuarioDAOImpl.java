042663628e8e25d7af1d76e41c95de4f9717b8c0
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-10-07 16:17:16 -0300

    Alterando import usuario dao

--- src/main/java/modelo/dao/usuario/UsuarioDAOImpl.java
+++ src/main/java/modelo/dao/usuario/UsuarioDAOImpl.java
@@ -14,6 +14,7 @@ import org.hibernate.Session;
 import modelo.entidade.usuario.UsuarioCadastrado;
 import modelo.entidade.usuario.UsuarioCadastrado_;
 import modelo.factory.conexao.ConexaoFactory;
+import modelo.dao.usuario.UsuarioDAO;
 
 public class UsuarioDAOImpl implements UsuarioDAO {
 
