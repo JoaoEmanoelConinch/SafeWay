61355fd71dc2653ff1425cec3f022ea62c9098fb
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-10-14 20:04:04 -0300

    id

--- src/main/java/modelo/entidade/usuario/UsuarioCadastrado.java
+++ src/main/java/modelo/entidade/usuario/UsuarioCadastrado.java
@@ -13,6 +13,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
